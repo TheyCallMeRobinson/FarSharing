3e74a619805570fe8be9e71f142d73fc5ee70666
TheyCallMeRobinson <alettertopaulrobinson0099@gmail.com>
2022-04-29 21:53:09 +0300

    Add activity Payment

---
+++ app/src/main/java/ru/vsu/cs/farsharing/PaymentActivity.java
@@ -0,0 +1,14 @@
+package ru.vsu.cs.farsharing;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+    }
+}
