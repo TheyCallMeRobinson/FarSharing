3c1aa20effd771332fc3a3656284d87a56329e9b
TheyCallMeRobinson <alettertopaulrobinson0099@gmail.com>
2022-04-30 12:24:03 +0300

    Create auto list activity

---
+++ app/src/main/java/ru/vsu/cs/farsharing/AutoListActivity.java
@@ -0,0 +1,14 @@
+package ru.vsu.cs.farsharing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AutoListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auto_list);
+    }
+}
