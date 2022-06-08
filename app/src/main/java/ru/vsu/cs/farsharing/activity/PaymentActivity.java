d7b8d43294e6066be61bcb563f7bd7ab2155d112
TheyCallMeRobinson <alettertopaulrobinson0099@gmail.com>
2022-06-08 05:12:12 +0300

    Add retrofit

--- app/src/main/java/ru/vsu/cs/farsharing/activity/PaymentActivity.java
+++ app/src/main/java/ru/vsu/cs/farsharing/activity/PaymentActivity.java
@@ -1,6 +1,9 @@
 package ru.vsu.cs.farsharing.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,10 +15,30 @@ public class PaymentActivity extends AppCompatActivity {
     private EditText cardNumber;
     private EditText validThru;
     private EditText cvv;
+    private Button submitPayment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+        setUpViews();
+        setUpListeners();
+    }
+
+    void setUpViews() {
+        cardNumber = findViewById(R.id.cardNumberEditText);
+        validThru = findViewById(R.id.validThruEditText);
+        cvv = findViewById(R.id.cvvEditText);
+        submitPayment = findViewById(R.id.submitPaymentButton);
+    }
+
+    void setUpListeners() {
+        submitPayment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toEmailConfirm = new Intent(PaymentActivity.this, ConfirmEmailActivity.class);
+                startActivity(toEmailConfirm);
+            }
+        });
     }
 }
---
+++ app/src/main/java/ru/vsu/cs/farsharing/config/AppConfig.java
@@ -0,0 +1,16 @@
+package ru.vsu.cs.farsharing.config;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public final class AppConfig {
+
+    public static final String baseUrl = "https://zachetka-server.herokuapp.com/";
+
+    public static final Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(new OkHttpClient.Builder().build())
+            .build();
+}
