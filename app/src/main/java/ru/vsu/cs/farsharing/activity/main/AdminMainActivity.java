531857e9b8760fa2a6cc2dc5d8026e85e95cbea6
TheyCallMeRobinson <alettertopaulrobinson0099@gmail.com>
2022-09-08 20:09:40 +0300

    Add admin activity

---
+++ app/src/main/java/ru/vsu/cs/farsharing/activity/main/AdminMainActivity.java
@@ -0,0 +1,14 @@
+package ru.vsu.cs.farsharing.activity.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_main);
+    }
+}
